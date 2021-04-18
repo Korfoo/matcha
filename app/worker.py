39d1f093a78285335edbad5485367b7ad91b5c9e
Korfoo <korfoo@telenet.be>
2021-04-18 08:50:40 +0200

    Add worker with basic logic to search for matches.

---
+++ app/worker.py
@@ -0,0 +1,20 @@
+import logging
+import time
+
+import redis
+
+logger = logging.getLogger(__name__)
+
+
+def publish_match(player_1_id, player_2_id, room_id):
+    pass
+
+
+def find_matches():
+    pass
+
+
+if __name__ == "__main__":
+    while True:
+        find_matches()
+        time.sleep(10)
