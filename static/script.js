4ed38aa06866a3711eeda3573e05e9d8b6b1991a
Korfoo <korfoo@telenet.be>
2021-04-20 21:33:03 +0200

    fix static index page

--- static/script.js
+++ static/script.js
@@ -1,7 +1,9 @@
 var ws = null;
 function connect(event) {
     var user = document.getElementById("id")
+    
     ws = new WebSocket("ws://0.0.0.0:8000/queue?user=" + user.value);
+    
     ws.onmessage = function (event) {
         var messages = document.getElementById('messages')
         var message = document.createElement('li')
