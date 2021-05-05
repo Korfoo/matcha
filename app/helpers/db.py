75ed316ed76fd2763ed69a3c5bebcdf82b4adb59
Korfoo <korfoo@telenet.be>
2021-05-05 23:13:46 +0200

    Close connections to redis te prevent error messages when match is found.

--- app/helpers/db.py
+++ app/helpers/db.py
@@ -7,10 +7,14 @@ logger = logging.getLogger("matcha")
 async def update_rating(user_id: str, rating: int):
     conn = await aioredis.create_connection("redis://redis", encoding="utf-8")
     await conn.execute("JSON.SET", user_id, ".rating", rating)
+    conn.close()
+
 
 async def update_games_played(user_id: str):
     conn = await aioredis.create_connection("redis://redis", encoding="utf-8")
     await conn.execute("JSON.NUMINCRBY", user_id, ".games_played", 1)
+    conn.close()
+
 
 
 async def add_user(user_id: str, username: str) -> dict:
@@ -19,6 +23,8 @@ async def add_user(user_id: str, username: str) -> dict:
     user = {"id": user_id, "username": username, "rating": 800, "games_played": 0}
     logger.debug(f"Adding {user} to database")
     await conn.execute("JSON.SET", user_id, ".", json.dumps(user))
+    conn.close()
+    await conn.wait_closed()
 
     return user
 
